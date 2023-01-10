efbac47462a60659d8777db7ac189873138946a3
gidonr <gidonr@battlelab.local>
2023-01-10 15:01:28 +0200

    Initial example client capable of talking on Root, and in theory listening

---
+++ MumbleClient.py
@@ -0,0 +1,77 @@
+import time
+
+import keyboard
+import pyaudio
+import pymumble_py3
+from pymumble_py3.callbacks import PYMUMBLE_CLBK_SOUNDRECEIVED as PCS
+
+# Example mumble command
+# self.mumble.commands.new_cmd(
+#     cmds.ModUserState(
+#         self.mumble.users.myself_session, {
+#             "session": self.mumble.users.myself_session,
+#             "listening_channel_add": self.mumble.channels.keys() ^ {0}
+#         }
+#     )
+# )
+
+# pyaudio constants
+CHUNKSIZE = 1024
+FORMAT = pyaudio.paInt16  # pymumble soundchunk.pcm is 16 bits
+PYAUDIO_CHANNELS = 1
+RATE = 48000  # pymumble soundchunk.pcm is 48000Hz
+
+
+class MumbleClient:
+    def __init__(self, server, nickname, pwd=""):
+        self.pwd = pwd
+        self.server = server
+        self.nickname = nickname
+
+        self.mumble: pymumble_py3.Mumble = None  # Defined in _create_mumble_instance
+        self.p: pyaudio.PyAudio = None  # Defined in _setup_audio
+        self.stream: pyaudio.Stream = None  # Defined in _setup_audio
+
+        self._setup_audio()
+        self._create_mumble_instance()
+        self._setup_keyboard_hooks()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+    def _create_mumble_instance(self):
+        self.mumble = pymumble_py3.Mumble(self.server, self.nickname, password=self.pwd)
+        self.mumble.callbacks.set_callback(PCS, self.sound_retriever_handler)
+        self.mumble.set_receive_sound(1)
+        self.mumble.start()
+        self.mumble.is_ready()
+
+    def _setup_audio(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=FORMAT,
+                                  channels=PYAUDIO_CHANNELS,
+                                  rate=RATE,
+                                  input=True,
+                                  output=True,
+                                  frames_per_buffer=CHUNKSIZE)
+
+    def _setup_keyboard_hooks(self):
+        keyboard.add_hotkey("`", self.audio_capture)
+
+    def sound_retriever_handler(self, user, soundchunk):
+        self.stream.write(soundchunk.pcm)
+
+    def audio_capture(self):
+        starting_time = time.time()
+        while time.time() - starting_time < 1:
+            data = self.stream.read(CHUNKSIZE, exception_on_overflow=False)
+            self.mumble.sound_output.add_sound(data)
+
+
+if __name__ == "__main__":
+    mumbler = MumbleClient("192.133.1.228", "John")
+
+    keyboard.wait("ctrl+esc")
+    print("Exited")
---
+++ main.py
@@ -0,0 +1,14 @@
+import keyboard
+
+
+
+
+
+
+
+
+
+
+# keyboard.add_hotkey("1", print, args=("triggered", "hotkey"))
+#
+# keyboard.wait("ctrl+shift+esc")
