d91e5575f2abb6641da0bdc887fbe1ba97b6df9e
gidonr <gidonr@battlelab.local>
2023-01-11 11:23:44 +0200

    Regularly update the configuration file

--- MumbleClient.py
+++ MumbleClient.py
@@ -1,8 +1,9 @@
 import json
 import time
-
 import keyboard
 import pyaudio
+import threading
+
 import pymumble_py3
 from pymumble_py3.callbacks import PYMUMBLE_CLBK_SOUNDRECEIVED as PCS
 from pymumble_py3.messages import MoveCmd
@@ -64,12 +65,6 @@ class MumbleClient:
                                   output=True,
                                   frames_per_buffer=CHUNKSIZE)
 
-    def _get_channel_id_from_name(self, channel_name: str):
-        for channel_id in self.mumble.channels.keys():
-            if self.mumble.channels[channel_id]["name"] == channel_name:
-                return channel_id
-        return 0
-
     def _setup_keyboard_hooks(self):
         keyboard.add_hotkey(self.configuration["speak"], self.audio_capture)
         person_type = None
@@ -83,9 +78,17 @@ class MumbleClient:
             for hook in self.configuration["configurations"][person_type]:
                 keyboard.add_hotkey(hook[0], self.change_channel, args=(hook[1],))
 
+    def update_configuration(self, configuration: dict):
+        self.configuration = configuration
+
+        keyboard.unhook_all()
+
+        self._setup_keyboard_hooks()
+
     def change_channel(self, target_channel_name):
         self.mumble.execute_command(
-            MoveCmd(self.mumble.users.myself_session, self._get_channel_id_from_name(target_channel_name)))
+            MoveCmd(self.mumble.users.myself_session,
+                    self.mumble.channels.find_by_name(target_channel_name)["channel_id"]))
 
     def sound_retriever_handler(self, user, soundchunk):
         self.stream.write(soundchunk.pcm)
@@ -97,11 +100,35 @@ class MumbleClient:
             self.mumble.sound_output.add_sound(data)
 
 
+def check_configuration_update(mumble_client: MumbleClient, configuration_path: str, last_update_time: float,
+                               refresh_time: int = 10):
+    update_time = os.path.getmtime(configuration_path)
+    if update_time > last_update_time:
+        print("update")
+        with open(configuration_path, 'r') as f:
+            configuration = json.load(f)
+        mumble_client.update_configuration(configuration)
+
+        next_thread = threading.Timer(refresh_time, check_configuration_update,
+                        args=(mumble_client, configuration_path, update_time, refresh_time))
+        next_thread.daemon = True
+        next_thread.start()
+
+    else:
+        print("no update")
+        next_thread = threading.Timer(refresh_time, check_configuration_update,
+                        args=(mumble_client, configuration_path, last_update_time, refresh_time))
+        next_thread.daemon = True
+        next_thread.start()
+
+
 if __name__ == "__main__":
     with open("example_config.json", 'r') as f:
         configuration = json.load(f)
-    # mumbler = MumbleClient("***REMOVED***", "John", configuration=configuration)
-    mumbler = MumbleClient("***REMOVED***", "***REMOVED***Only", configuration=configuration)
+    mumbler = MumbleClient("***REMOVED***", "John", configuration=configuration)
+    # mumbler = MumbleClient("***REMOVED***", "***REMOVED***Only", configuration=configuration)
+
+    check_configuration_update(mumbler, "example_config.json", os.path.getmtime("example_config.json"), 2)
 
     keyboard.wait("esc")
     print("Exited")
