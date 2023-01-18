140f46d5cc6ade30ef4f138277e75e08467386bc
gidonr <gidonr@battlelab.local>
2023-01-18 08:36:47 +0200

    Minor adjustments, mainly focused around making this work on machines without opus installed
    The version of opuslib has been adjusted. In api/__init__.py, the Dll is loaded from this base directory, rather than from the system.

--- build_exe.py
+++ build_exe.py
@@ -2,6 +2,9 @@ import PyInstaller.__main__
 
 PyInstaller.__main__.run([
     "main.py",
+    r'--add-binary ".\opus.dll;."',
     "--noconsole",
     "--onefile"
 ])
+
+# pyinstaller.exe .\main.py --add-binary ".\opus.dll;." --onefile
